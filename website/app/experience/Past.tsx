8752225de91bc8f2c87da05a0fc198fccb844e03
Tim Tran <49887921+tacascer@users.noreply.github.com>
2023-07-16 23:12:45 -0400

    refactor: Made `Past` a server component

--- website/app/experience/Past.tsx
+++ website/app/experience/Past.tsx
@@ -1,44 +1,12 @@
-"use client";
-
-import { Col, Row } from "react-bootstrap";
 import Header from "@/components/Header";
 import Section from "@/components/Section";
-import EmployerCard, { EmployerCardProps } from "./EmployerCard";
-
-const PREVIOUS_EMPLOYERS: EmployerCardProps[] = [
-  {
-    name: "TradeWeb",
-    description: `
-    Tradeweb offers electronic over-the-counter (OTC) marketplaces for trading fixed income products, ETFs, and derivatives.
-    The company offers market participants unparalleled liquidity and a broad range of data solutions. 
-    `,
-    link: "https://www.tradeweb.com/",
-    image: "/tradeweb.svg",
-    altText: "The Tradeweb Logo",
-  },
-  {
-    name: "Amazon Robotics",
-    description: `
-    Amazon Robotics is a subsidiary of Amazon that designs, manufactures, and operates robots used in fulfillment centers.
-     The company's robots are used to move products around warehouses and sort them for shipping. 
-  `,
-    link: "https://www.amazon.jobs/en/teams/amazon-robotics",
-    image: "/amazon-robotics.svg",
-    altText: "The Amazon Robotics Logo",
-  },
-];
+import PastGrid from "./PastGrid";
 
 const Past = () => {
   return (
     <Section>
       <Header>Wrote &#127837; Code At</Header>
-      <Row>
-        {PREVIOUS_EMPLOYERS.map((employer, index) => (
-          <Col key={index}>
-            <EmployerCard {...employer}></EmployerCard>
-          </Col>
-        ))}
-      </Row>
+      <PastGrid></PastGrid>
     </Section>
   );
 };
---
+++ website/app/experience/PastGrid.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Row, Col } from "react-bootstrap";
+import EmployerCard, { EmployerCardProps } from "./EmployerCard";
+
+const PREVIOUS_EMPLOYERS: EmployerCardProps[] = [
+  {
+    name: "TradeWeb",
+    description: `
+    Tradeweb offers electronic over-the-counter (OTC) marketplaces for trading fixed income products, ETFs, and derivatives.
+    The company offers market participants unparalleled liquidity and a broad range of data solutions. 
+    `,
+    link: "https://www.tradeweb.com/",
+    image: "/tradeweb.svg",
+    altText: "The Tradeweb Logo",
+  },
+  {
+    name: "Amazon Robotics",
+    description: `
+    Amazon Robotics is a subsidiary of Amazon that designs, manufactures, and operates robots used in fulfillment centers.
+     The company's robots are used to move products around warehouses and sort them for shipping. 
+  `,
+    link: "https://www.amazon.jobs/en/teams/amazon-robotics",
+    image: "/amazon-robotics.svg",
+    altText: "The Amazon Robotics Logo",
+  },
+];
+
+const PastGrid = () => {
+  return (
+    <Row>
+      {PREVIOUS_EMPLOYERS.map((employer, index) => (
+        <Col key={index}>
+          <EmployerCard {...employer}></EmployerCard>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default PastGrid;
