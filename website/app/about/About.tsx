59d83d79a644d978f04b98eaa3c7322b51f40b8a
Tim Tran <49887921+tacascer@users.noreply.github.com>
2023-07-18 00:38:55 -0400

    Center text in About

--- website/app/about/About.tsx
+++ website/app/about/About.tsx
@@ -2,27 +2,25 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <>
-      <header
-        id="about"
-        className="p-5 my-5 text-center text-bg-primary bg-opacity-75 bg-gradient rounded-5"
-      >
-        <Image
-          className="d-block mx-auto mb-4 rounded-circle"
-          src="/profile.jpeg"
-          alt="Picture of yours truly"
-          width={200}
-          height={200}
-        />
-        <h1 className="display-5 fw-bold ">Hi, I&apos;m Tim</h1>
-      </header>
+    <header
+      id="about"
+      className="p-5 my-5 text-center text-bg-primary bg-opacity-75 bg-gradient rounded-5"
+    >
+      <Image
+        className="d-block mx-auto mb-4 rounded-circle"
+        src="/profile.jpeg"
+        alt="Picture of yours truly"
+        width={200}
+        height={200}
+      />
+      <h1 className="display-5 fw-bold ">Hi, I&apos;m Tim</h1>
       <main className="mx-auto m-3">
         <p className="fw-light fs-4 ">Developer, learner, and opinion-haver</p>
         <p className="fw-light fs-4">
           I read books, build things, and test in production
         </p>
       </main>
-    </>
+    </header>
   );
 };
 
