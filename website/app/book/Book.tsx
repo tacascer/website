31aea515511e84ba5a73849f18d5cf6385792a69
Tim Tran <49887921+tacascer@users.noreply.github.com>
2023-07-22 01:10:32 -0400

    Update reviews on books

--- website/app/book/Book.tsx
+++ website/app/book/Book.tsx
@@ -11,6 +11,37 @@ async function getBooks() {
 
   return res.json();
 }
+
+const REVIEWS: {
+  title: string;
+  review: string;
+}[] = [
+  {
+    title: "A Random Walk Down Wall Street",
+    review:
+      "The only investing book you'll ever need to read. Never again trust investment advice on TikTok.",
+  },
+  {
+    title: "Sapiens",
+    review:
+      "At some point, Homo Sapiens went through the Cognitive Revolution. We then built the Internet and put cat videos on it.",
+  },
+  {
+    title: "Atomic Habits",
+    review:
+      "Do I believe that humans can actually improve exponentially? No, but this book should motivate you to at least improve linearly.",
+  },
+  {
+    title: "Factfulness",
+    review:
+      "The world is not as bad as we think it is. Also, a lot of problems arise not from cultural and geographic difference, but from wealth disparity #marxwasright",
+  },
+  {
+    title: "Thinking, Fast and Slow",
+    review:
+      "Nobel laureate Daniel Kahneman will detail his research on the irrationality of the mind while also reading yours the entire time.",
+  },
+];
 const Book = async () => {
   const books = await getBooks();
 
@@ -28,7 +59,7 @@ function toBooks(books: any) {
       id: book.id,
       title: book.volumeInfo.title,
       author: toAuthor(book.volumeInfo.authors),
-      description: book.volumeInfo.description,
+      description: getReview(book.volumeInfo.title),
       image: book.volumeInfo.imageLinks.thumbnail,
       infoLink: book.volumeInfo.infoLink,
     };
@@ -37,6 +68,10 @@ function toBooks(books: any) {
 
 function toAuthor(authors: string[]) {
   return authors.join(", ");
+}
+
+function getReview(title: string) {
+  return REVIEWS.find((review) => review.title === title)?.review;
 }
 
 export default Book;
