c377fa91205a812083f26c5fd390e9170f86cab8
Tim Tran <49887921+tacascer@users.noreply.github.com>
2023-07-22 23:57:27 -0400

    Make authors  an array

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -17,10 +17,12 @@ export const metadata = {
   title: "Tim's Corner",
   description:
     "Tim Tran (@tacascer) is a software engineer based in NYC. He is passionate about building software that is scalable, maintainable, and easy to use. This is his corner of the internet.",
-  authors: {
-    name: "Tim Tran",
-    alias: "tacascer",
-  },
+  authors: [
+    {
+      name: "Tim Tran",
+      alias: "tacascer",
+    },
+  ],
 };
 
 export default function RootLayout({
