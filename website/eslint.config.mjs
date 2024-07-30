db7bb1cae773095ab2ac7ccc2bd6d3f3928f8f61
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-29 23:46:42 -0400

    deps: update eslint version

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -1,15 +1,8 @@
 import eslint from "@eslint/js";
-import next from "@next/eslint-plugin-next";
 import eslintConfigPrettier from "eslint-config-prettier";
+
 export default [
   eslint.configs.recommended,
   eslintConfigPrettier,
-  {
-    plugins :{
-      "@next/next": next,
-    }
-    rules: {
-      ...next.configs.recommended.rules,
-    },
-  }
+  {files: ["**/*.ts"]}
 ]
