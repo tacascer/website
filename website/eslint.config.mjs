ed3b58ea323efaf9756bb1ee85d3d022c55fcb0e
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-08-01 01:10:38 -0400

    chore: set up eslint for ts

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -1,8 +1,10 @@
 import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [
+export default tseslint.config(
   eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   eslintConfigPrettier,
   { files: ["**/*.ts", "**/*.tsx"] },
-];
+);
